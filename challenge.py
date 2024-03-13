69d744682c6d45323ef15bc9f3677fcbd03247e8
Josh Gray <joshgray91@gmail.com>
2024-03-13 10:31:33 +0000

    Implement add_prefix_un.

---
+++ challenge.py
@@ -0,0 +1,4 @@
+"""Functions to correctly transform words with either a prefix or suffix."""
+
+def add_prefix_un(word: str) -> str:
+    return f"un{word}"
--- challenge_test.py
+++ challenge_test.py
@@ -1,1 +1,11 @@
-"""Functions to correctly transform words with either a prefix or suffix."""
+"""Test functions in challenge.py."""
+from challenge import add_prefix_un
+
+def test_add_prefix_un_returns_string() -> None:
+    """Test that add_prefix_un returns a string."""
+    assert isinstance(add_prefix_un("test"), str)
+
+def test_add_prefix_un_returns_expected() -> None:
+    """Test that add_prefix_un returns expected string."""
+    assert add_prefix_un("happy") == "unhappy"
+    assert add_prefix_un("manageable") == "unmanageable"
