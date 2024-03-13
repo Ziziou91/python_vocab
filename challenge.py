e0a72a684f530219fb09fbf05987ba3903eabff8
Josh Gray <joshgray91@gmail.com>
2024-03-13 10:48:58 +0000

    Implement make_word_groups.

--- challenge.py
+++ challenge.py
@@ -1,4 +1,19 @@
 """Functions to correctly transform words with either a prefix or suffix."""
 
 def add_prefix_un(word: str) -> str:
+    """Adds the prefix 'un' to a given word."""
     return f"un{word}"
+
+def make_word_groups(words: list) -> str:
+    """Adds a prefix to all words in a list. Words list will be in the following format.
+    [<prefix>, <word_1>, <word_2> .... <word_n>]"""
+    prefix = words[0]
+    result_list = []
+
+    for count, word in enumerate(words):
+        if count == 0:
+            result_list.append(word)
+        else:
+            result_list.append(f"{prefix}{word}")
+
+    return " :: ".join(result_list)
--- challenge_test.py
+++ challenge_test.py
@@ -1,6 +1,8 @@
 """Test functions in challenge.py."""
-from challenge import add_prefix_un
+import pytest
+from challenge import add_prefix_un, make_word_groups
 
+# ===========test add_prefix_un===========
 def test_add_prefix_un_returns_string() -> None:
     """Test that add_prefix_un returns a string."""
     assert isinstance(add_prefix_un("test"), str)
@@ -9,3 +11,23 @@ def test_add_prefix_un_returns_expected() -> None:
     """Test that add_prefix_un returns expected string."""
     assert add_prefix_un("happy") == "unhappy"
     assert add_prefix_un("manageable") == "unmanageable"
+
+
+# ===========test make_word_groups===========
+def test_make_word_groups_returns_string() -> None:
+  """Test that make_word_groups returns a string."""
+  assert isinstance(make_word_groups(["un", "believeavle", "remarkable", "ending"]), str)
+
+@pytest.mark.parametrize(
+      ("words", "expected"),
+      [
+         (["en", "close", "joy", "lighten"], "en :: enclose :: enjoy :: enlighten"),
+         (["pre", "serve", "dispose", "position"], "pre :: preserve :: predispose :: preposition"),
+         (["auto", "didactic", "graph", "mate"], "auto :: autodidactic :: autograph :: automate"),
+         (["inter", "twine", "connected", "dependent"], "inter :: intertwine :: interconnected :: interdependent")
+
+      ]
+)
+def test_make_word_groups_returns_expected(words: list, expected: str) -> None:
+  """Test that make_word_groups returns expected string."""
+  assert make_word_groups(words) == expected
